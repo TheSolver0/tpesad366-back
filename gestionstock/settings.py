edb2c37c9f9da2bda5ce22b536b15738efe95a77
TheSolver0 <lucfotso0@gmail.com>
2025-04-23 23:33:08 +0200

    tentative de migration sur workbrench

--- gestionstock/settings.py
+++ gestionstock/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
      # Ajout des cors pour react
     "corsheaders",
+    'import_export',
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -84,9 +85,17 @@ WSGI_APPLICATION = 'gestionstock.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tpesadbd',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost', 
+        'PORT': '3306',
     }
 }
 
